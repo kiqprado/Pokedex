1d7ececd7af8366bf11c17f88ea96a3300315c17
Kaique Prado <kiqprado@outlook.com>
2023-04-16 12:46:42 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+const pokemonName = document.querySelector('.pokemon-name')
+const pokemonNumber = document.querySelector('.pokemon-number')
+const pokemonImage = document.querySelector('.pokemon-img')
+
+const form = document.querySelector('.form')
+const input = document.querySelector('.input-search')
+const buttonPrev = document.querySelector('btn-prev')
+const buttonNext = document.querySelector('btn-next')
+
+let searchPokemon = 202
+
+const fetchPokemon = async pokemon => {
+  const APIResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+  )
+  if (APIResponse.status === 200) {
+    const data = await APIResponse.json()
+
+    return data
+  }
+}
+
+const renderPokemon = async pokemon => {
+  pokemonName.innerHTML = 'Procurando... '
+  pokemonNumber.innerHTML = ''
+
+  const data = await fetchPokemon(pokemon)
+
+  if (data) {
+    pokemonImage.src =
+      data['sprites']['versions']['generation-v']['black-white']['animated'][
+        'front_default'
+      ]
+    pokemonImage.style.display = 'block'
+    pokemonName.innerHTML = data.name
+    pokemonNumber.innerHTML = data.id
+
+    input.value = ''
+  } else {
+    pokemonImage.style.display = 'none'
+    pokemonName.innerHTML = 'Não Existe.'
+    pokemonNumber.innerHTML = ''
+  }
+}
+
+form.addEventListener('submit', event => {
+  event.preventDefault()
+  renderPokemon(input.value.toLowerCase())
+})
+
+
+//             Não Concluido Ainda          //
+
+// buttonPrev.addEventListener('click', () => {
+// alert('prev click')
+// })
+
+// buttonNext.addEventListener('click', () => {
+//  alert('next click')
+// }) 
+
+renderPokemon(searchPokemon)
